06bb891e3c162a03665c60b86337e414dad8edf9
Eunjin0910 <gurwls2399@naver.com>
2019-05-20 12:18:25 +0900

    DB 접속 정보 변경  - 로컬 DB -> 개인 DB 로 변경
    Entity Table Name 변경
     - user -> user_dev

--- src/main/java/com/example/springbootrestapi/entity/User.java
+++ src/main/java/com/example/springbootrestapi/entity/User.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user_dev")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class User {
 
